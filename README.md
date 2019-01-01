c25f5cb70e87dffc465d1721ee7ff81c85c90ac1
Bram Goolaerts <bram@goolaerts.be>
2019-01-01 20:30:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,103 @@
+# delijn-card
+
+This card generates a passages card for the De Lijn public transport service in Flanders (Belgium).
+
+![example](example.png)
+
+## Options
+
+| Name | Type | Requirement | Description
+| ---- | ---- | ------- | -----------
+| type | string | **Required** | `custom:delijn-card`
+| entity | string | **Required** | The entity_id of the entity you want to show.
+| title | string | **Optional** | Add a custom title to the card.
+| config_type | string | **Optional** | 'default' for standard setup, 'raw' for raw entity data, 'columns' for defining a custom list of columns. If this option is not added the default setup will be shown in the card.
+| columns | object | **Optional** | List of columns to display.
+
+## Column object
+
+| Name | Type | Requirement | Description
+| ---- | ---- | ------- | -----------
+| title | string | **Required** | Column header to display.
+| field | string | **Required** | key value of the entity that you wish to display.
+
+## Installation
+
+### Step 1
+
+Install `delijn-card` by copying `delijn-card.js` from this repo to `<config directory>/www/delijn-card.js` of your Home Assistant instance.
+
+**Example:**
+
+```bash
+wget https://github.com/bollewolle/delijn-card/raw/master/delijn-card.js
+mv delijn-card.js ~/.homeassistant/www/
+```
+
+### Step 2
+
+Set up the De Lijn custom sensor.
+
+**Example:**
+
+```yaml
+sensor:
+  - platform: delijn
+    sub_key: '<put your data.delijn.be subscriptionkey here>'
+    nextpassage:
+    - stop_id: '200552'
+      max_passages: 10
+```
+**_Note_**: replace with the subscription key you generated with you data.delijn.be developer account.
+
+### Step 3
+
+Link `delijn-card` inside you `ui-lovelace.yaml`.
+
+```yaml
+resources:
+  - url: /local/delijn-card.js
+    type: js
+```
+
+### Step 4
+
+Add a custom element in your `ui-lovelace.yaml`
+
+**Example:**
+
+Example with default layout:
+```yaml
+      - type: 'custom:delijn-card'
+        entity: sensor.korenmarkt_perron_2_gent
+        title: 'Korenmarkt Perron 2, Gent'
+
+```
+![example](example.png)
+
+```yaml
+      - type: 'custom:delijn-card'
+        entity: sensor.brugsepoort_gent
+        title: 'Brugsepoort, Gent'
+        config_type: columns
+        columns:
+          - field: line_number_public
+            title: Line
+          - field: line_transport_type
+            title: Type
+          - field: final_destination
+            title: Towards
+          - field: due_in_min
+            title: Due in (min)
+          - field: due_at_rt
+            title: Due at (real-time)
+          - field: due_at_sch
+            title: Due at (scheduled)
+
+```
+![example](example2.png)
+
+
+## Credits
+
+Thanks to [DavidMStraub](https://github.com/DavidMStraub) for all his initial work.
---
+++ delijn-card.js
@@ -0,0 +1,207 @@
+class DeLijnCard extends HTMLElement {
+
+    constructor() {
+      super();
+      this.attachShadow({ mode: 'open' });
+    }
+
+    setConfig(config) {
+      if (!config.entity) {
+        throw new Error('Please define an entity');
+      }
+
+      const root = this.shadowRoot;
+      if (root.lastChild) root.removeChild(root.lastChild);
+
+      const cardConfig = Object.assign({}, config);
+      const columns = cardConfig.columns;
+      const card = document.createElement('ha-card');
+      const content = document.createElement('div');
+      const style = document.createElement('style');
+      style.textContent = `
+            ha-card {
+              /* sample css */
+            }
+            table {
+              width: 100%;
+              padding: 0 32px 0 32px;
+              text-align: left;
+            }
+            thead th {
+              text-align: left;
+            }
+            tbody tr {
+              background-color: var(--paper-card-background-color);
+            }
+            td a {
+              color: var(--primary-text-color);
+              text-decoration-line: none;
+              font-weight: normal;
+            }
+            .line-number{
+              display:block;
+              float:left;
+              min-width:2.75em;
+              text-align:center;
+              font-weight:bold;
+              color:#fff;
+              border-radius:4px;
+              border:3px solid #000;
+              margin-left:0px;
+              margin-right:12px;
+              margin-left:0rem;
+              margin-right:0.75rem;
+              font-size:12px;
+              font-size:0.75rem;
+              line-height:1.6em
+            }
+          `;
+
+      content.id = "container";
+      cardConfig.title ? card.header = cardConfig.title : null;
+      card.appendChild(content);
+      card.appendChild(style);
+      root.appendChild(card);
+      this._config = cardConfig;
+    }
+
+    set hass(hass) {
+      const config = this._config;
+      const root = this.shadowRoot;
+      const card = root.lastChild;
+
+      if (hass.states[config.entity]) {
+        const feed = hass.states[config.entity].attributes['next_passages'];
+        const config_type = config.config_type;
+        var columns = config.columns;
+        this.style.display = 'block';
+        const rowLimit = config.row_limit ? config.row_limit : Object.keys(feed).length;
+        let rows = 0;
+
+        if (config_type == "default" || !config_type) {
+          // set a default configuration
+          columns = [{'field': 'line_number_public', 'title': 'Line'},
+                     {'field': 'line_transport_type', 'title': 'Type'},
+                     {'field': 'final_destination', 'title': 'Towards'},
+                     {'field': 'due_in_min', 'title': 'Due in (min)'}];
+        } else if (config_type == "columns" && !columns) {
+          columns = config.columns;
+        } else if (config_type == "raw" || !columns) {
+          columns = null;
+        }
+
+        // First: define the header (1st row OR taken from config)
+        if (feed !== undefined && Object.keys(feed).length > 0) {
+          let card_content = '<table><thread><tr>';
+
+          if (!columns) {
+            card_content += `<tr>`;
+            // in case no columns are selected in the config then we
+            // print all the technical names of the columns as header line
+            for(var key in feed[0]) {
+              if(feed[0].hasOwnProperty(key)) { //to be safe
+                card_content += `<th>${key}</th>`;
+              }
+            }
+          } else {
+            for (let column in columns) {
+              if (columns.hasOwnProperty(column)) {
+                card_content += `<th>${columns[column].title}</th>`;
+              }
+            }
+          }
+
+          card_content += `</tr></thead><tbody>`;
+
+          // Next: define the body/contents (all rows or only those in config)
+          for (let entry in feed) {
+            if (rows >= rowLimit) break;
+
+            var bordercol = '#'+feed[entry]['line_number_colourBackBorderHex']
+            var backcol = '#'+feed[entry]['line_number_colourBackHex']
+            var color = '#'+feed[entry]['line_number_colourFrontHex']
+
+            if (feed.hasOwnProperty(entry)) {
+              // if there's no columns in config defined then print all
+              if (!columns) {
+                for (let field in feed[entry]) {
+                  if (feed[entry].hasOwnProperty(field)) {
+                    // show proper line number format of De Lijn for the line_number_public
+                    if (field == 'line_number_public') {
+                      card_content += `<td class= "line-number" style = "border-color: ${bordercol}; background-color: ${backcol}; color: ${color};">${feed[entry][field]}</td>`;;
+                    } else {
+                      card_content += `<td>${feed[entry][field]}</td>`;
+                    }
+                  }
+                }
+              // else we print only the selected columns
+              } else {
+                let has_field = true;
+
+                for (let column in columns) {
+                  if (!feed[entry].hasOwnProperty(columns[column].field)) {
+                    has_field = false;
+                    break;
+                  }
+                }
+
+                if (!has_field) continue;
+                card_content += `<tr>`;
+
+                for (let column in columns) {
+                  if (columns.hasOwnProperty(column)) {
+                    // show proper line number format of De Lijn for the line_number_public
+                    if(columns[column].field == 'line_number_public') {
+                      card_content += `<td class= "line-number" style = "border-color: ${bordercol}; background-color: ${backcol}; color: ${color};">`;
+                      let newText = feed[entry][columns[column].field];
+                      card_content += `${newText}`;
+                    } else if(columns[column].field == 'line_transport_type') {
+                      // ideally we show icons here for the different types (not yet implemented)
+                      card_content += `<td class=${columns[column].field}>`;
+                      let newText = feed[entry][columns[column].field];
+                      card_content += `${newText}`;
+                    } else if (columns[column].field == 'due_at_sch' || columns[column].field == 'due_at_rt') {
+                      card_content += `<td class=${columns[column].field}>`;
+                      let newText = feed[entry][columns[column].field];
+                      if (newText !== null) {
+                        // change string to correct datetime with timezone offset
+                        var dt = new Date(newText + "+01:00");
+                        // only retrieve the hh:mm and not the seconds (which are always 00)
+                        newText = dt.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");;
+                        card_content += `${newText}`;
+                      } else {
+                        newText = ""
+                        card_content += `${newText}`;
+                      }
+                    } else {
+                      card_content += `<td class=${columns[column].field}>`;
+                      let newText = feed[entry][columns[column].field];
+                      card_content += `${newText}`;
+                    }
+                    card_content += `</td>`;
+                  }
+                }
+              }
+
+              card_content += `</tr>`;
+              ++rows;
+            }
+          }
+
+          root.lastChild.hass = hass;
+          card_content += `</tbody></table>`;
+          root.getElementById('container').innerHTML = card_content;
+        } else {
+          this.style.display = 'none';
+        }
+      } else {
+        this.style.display = 'none';
+      }
+    }
+
+    getCardSize() {
+      return 1;
+    }
+  }
+
+customElements.define('delijn-card', DeLijnCard)
